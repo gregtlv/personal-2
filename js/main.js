2460d3a7f34e9161693531fd99a65b60932719a3
Greg Mac <manov@manov.ru>
2018-04-13 11:43:26 +0300

    Form Validation

---
+++ js/main.js
@@ -0,0 +1,107 @@
+// Greg
+
+// headerBurger.onclick = function expandMenu() {
+//   var headerNav = document.getElementById('headerNav');
+//
+//   let headerNavClass = headerNav.classList;
+//
+//   console.log(headerNavClass);
+//
+//
+//   if (headerNav.className === "header-top__nav") {
+//       headerNav.className += " responsive";
+//   } else {
+//     headerNavClass.remove("responsive");
+//   }
+//
+//   console.log(headerNavClass);
+//
+// }
+
+sendButton.onclick = function sendForm(event) { //event важная вещь для Firefox
+
+	event.preventDefault();
+
+	var userName = document.getElementById('inputName');
+	var userNumber = document.getElementById('inputPhone');
+	var userMail = document.getElementById('inputEmail');
+	var userText = document.getElementById('inputText');
+
+	var alertMessage = document.getElementById('alertMessage');
+
+
+
+	let userName_Class = userName.classList;
+	let userNumber_Class = userNumber.classList;
+	let userMail_Class = userMail.classList;
+	let userText_Class = userText.classList;
+
+	let alertMessage_Class = alertMessage.classList;
+
+  // User Name
+
+	if (userName.value == "") {
+		userName_Class.add("pink", "lighten-2");
+
+	}
+	else {
+		userName_Class.remove("pink", "lighten-2");
+
+	}
+
+  // User Number
+
+	if (userNumber.value == "") {
+		userNumber_Class.add("pink", "lighten-2");
+
+	}
+	else {
+		userNumber_Class.remove("pink", "lighten-2");
+
+	}
+
+  // User Email
+
+	if (userMail.value == "") {
+		userMail_Class.add("pink", "lighten-2");
+
+	}
+	else {
+		userMail_Class.remove("pink", "lighten-2");
+
+	}
+
+
+	// Text Message
+
+	if (userText.value == "") {
+		userText_Class.add("pink", "lighten-2");
+		console.log('Text mesage here');
+
+	}
+	else {
+		userText_Class.remove("pink", "lighten-2");
+
+	}
+
+
+	console.log(userName.classList);
+	console.log(userMail.classList);
+	console.log(userText.classList);
+
+
+	if (userName.value != "" && userNumber.value != "" && userMail.value != "" && userText.value != "") {
+		// form.style.display = "none";
+		// classNamesForm.remove("form");
+		console.log('Form sent');
+    alertMessage.style.opacity = "0";
+    sendButton.style.margin = "20px auto auto auto";
+		alert("Форма отправки пока что не подключена");
+	}
+  else {
+		alertMessage.style.opacity = "1";
+    sendButton.style.margin = "20px auto 15px auto";
+
+	}
+
+}
